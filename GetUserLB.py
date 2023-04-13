d0fc453771c7dc96ebe823475ce1d1f88aa4fd0a
GonGe1018 <chaeho1234@naver.com>
2023-04-13 15:30:27 +0900

    디스코드 봇 추가

---
+++ GetUserLB.py
@@ -0,0 +1,61 @@
+import requests
+import json
+
+
+def Get(userName):
+    LBdict = {
+        'name':'',
+        'ts':'',
+        '40L':'',
+        'blitz':'',
+        'league':
+        {
+            'playeds':'',
+            'wins':'',
+            'rank':''    
+        }
+    }
+    r = requests.get('https://ch.tetr.io/api/users/'+userName)
+    html = r.text
+    userInfo_json=json.loads(html)
+
+    r = requests.get('https://ch.tetr.io/api/users/'+userName+'/records')
+    html = r.text
+    userRecord_json=json.loads(html)
+
+    #유저 이름
+    try:
+        LBdict['name']=userInfo_json['data']['user']['username']
+    except KeyError:
+        return('no user')
+    #print('유저 이름 : '+LBdict['name'])
+
+    #유저 가입일
+    LBdict['ts']=userInfo_json['data']['user']['ts']
+    #print('유저 가입일 : '+ LBdict['ts'])
+
+    #40라인 최고 기록
+    try:
+        LBdict['40L']=int(userRecord_json['data']['records']['40l']['record']['endcontext']['finalTime'])//1000 #bestRecord
+    except TypeError:
+        LBdict['40L']='null'
+    #print('유저 40L 최고 기록 : %s분 %s초 ' %(LBdict['40L']//60,LBdict['40L']%60))
+
+    #blitz 최고 기록
+    try:
+        LBdict['blitz']=userRecord_json['data']['records']['blitz']['record']['endcontext']['score']
+    except TypeError:
+        LBdict['blitz']='null'
+    #print('유저 blitz 최고 기록 :',LBdict['blitz'])
+
+    #리그 기록
+    LBdict['league']['played']=userInfo_json['data']['user']['league']['gamesplayed']
+    LBdict['league']['wins']=userInfo_json['data']['user']['league']['gameswon']
+    LBdict['league']['rank']=userInfo_json['data']['user']['league']['rank']
+    #print('유저 리그 기록\n  플레이한 게임 : %s\n  이긴 게임 : %s\n  랭크 : %s'%(LBdict['league']['played'],LBdict['league']['wins'],LBdict['league']['rank']))
+
+    return(LBdict)
+
+
+
+
--- main.py
+++ main.py
@@ -1,43 +1,19 @@
-import requests
-import json
+import discord
 
+client = discord.Client()
 
-def GetUserLB(userName):
-    r = requests.get('https://ch.tetr.io/api/users/'+userName)
-    html = r.text
-    userInfo_json=json.loads(html)
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
 
-    r = requests.get('https://ch.tetr.io/api/users/'+userName+'/records')
-    html = r.text
-    userRecord_json=json.loads(html)
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
-    #유저 이름
-    print('유저 이름 : '+userInfo_json['data']['user']['username'])
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
 
-    #유저 가입일
-    user_ts=userInfo_json['data']['user']['ts']
-    print('유저 가입일 : '+ user_ts)
-
-    #40라인 최고 기록
-    try:
-        user_40L=int(userRecord_json['data']['records']['40l']['record']['endcontext']['finalTime'])//1000 #bestRecord
-    except TypeError:
-        user_40L='null'
-    print('유저 40L 최고 기록 : %s분 %s초 ' %(user_40L//60,user_40L%60))
-
-    #blitz 최고 기록
-    try:
-        user_blitz=int(userRecord_json['data']['records']['blitz']['record']['endcontext']['score'])
-    except TypeError:
-        user_blitz='null'
-    print('유저 blitz 최고 기록 :',user_blitz)
-
-    #리그 기록
-    user_league=[userInfo_json['data']['user']['league']['gamesplayed'],userInfo_json['data']['user']['league']['gameswon'],userInfo_json['data']['user']['league']['rank']]
-    print('유저 리그 기록\n  플레이한 게임 : %s\n  이긴 게임 : %s\n  랭크 : %s'%(user_league[0],user_league[1],user_league[2]))
-
-
-string = input()
-GetUserLB(string)
+client.run('MTA5NTkxODI1NTQwODkzOTAyOQ.GvtwR4.wLjYz6XUngChN-gRPoxDQWHmiIF_Y6s8IunKoc')
 
 
